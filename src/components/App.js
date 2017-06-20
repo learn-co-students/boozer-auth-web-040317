0b456788173a99d8b5d003d99f75cb16c36eb5b8
aisleypay <paisley.singh@gmail.com>
2017-06-20 14:27:55 -0400

    done!

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,74 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 
 import CocktailsPageContainer from '../containers/CocktailsPageContainer'
+import LoginForm from './LoginForm'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      auth: {
+        isLoggedIn: false,
+        user: {}
+      }
+    }
+
+    this.logIn = this.logIn.bind(this)
+  }
+
+componentDidMount() {
+  if(localStorage.getItem('user_id')) {
+    fetch('http://localhost:3000/api/v1/current_user', {
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'Authorization': localStorage.getItem('user_id')
+      }
+    })
+    .then(res => res.json())
+    .then(user => this.setState({
+      auth: {
+        isLoggedIn: true,
+        user: user
+      }
+    }))
+  }
+}
+
+logIn(loginParms) {
+  fetch('http://localhost:3000/api/v1/auth', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+      'Authorization': localStorage.getItem('user_id')
+    },
+    body: JSON.stringify(loginParms)
+  }).then(res => res.json())
+  .then(user => {
+    if (!user.error) {
+      this.setState({
+        auth: {
+          isLoggedIn: true,
+          user: user
+        }
+      })
+      localStorage.setItem('user_id', user.id)
+    }
+  })
+
+}
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Boozer</h2>
+          <h2>Welcome to Boozer {this.state.auth.user.username}</h2>
           <Link to='/cocktails'>See All Cocktails</Link>
           <Link to='/'>Home</Link>
+          <Link to='/login'>Log In</Link>
         </div>
         <Route path="/cocktails" component={CocktailsPageContainer} />
+        <Route path="/login" render={() => <LoginForm onSubmit={this.logIn} />} />
       </div>
     );
   }
